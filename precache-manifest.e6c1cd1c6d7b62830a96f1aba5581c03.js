bcc673657afc83bea19183772c33d7d0a879e9f8
vineetk13 <vineetkme.99@gmail.com>
2020-10-11 00:29:12 +0530

    Updates

---
+++ precache-manifest.e6c1cd1c6d7b62830a96f1aba5581c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43054497ac543227e1103b2bce69701",
+    "url": "/zscore-dashboard-ui/index.html"
+  },
+  {
+    "revision": "5e1dbba9ee3a5690b4e6",
+    "url": "/zscore-dashboard-ui/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3cba4582b2e12e59cd24",
+    "url": "/zscore-dashboard-ui/static/css/main.00b9cdd5.chunk.css"
+  },
+  {
+    "revision": "5e1dbba9ee3a5690b4e6",
+    "url": "/zscore-dashboard-ui/static/js/2.3792df05.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/zscore-dashboard-ui/static/js/2.3792df05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cba4582b2e12e59cd24",
+    "url": "/zscore-dashboard-ui/static/js/main.dd16fe26.chunk.js"
+  },
+  {
+    "revision": "abe025ced7ce7d0dc2a4",
+    "url": "/zscore-dashboard-ui/static/js/runtime-main.c87154c8.js"
+  }
+]);
